7525be49ea44e22549605f62b348b5fe6e6c9ea1
Travis Umbel <Umbelth94@gmail.com>
2024-01-08 20:56:35 -0600

    added basic psuedo notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-console.log('Hello world!');
+//Fetch TMDB API
+    //Display basic info
+    //Display advanced info on button press
+
+
+//Fetch Ebay API
+    //Selectors help refine search data
+    //Display merch from results for movie input
+
+//Recieve Input Data from search bar
+
+//Saves movies locally
+
+//Displays locally saved movies
+
+  
+fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=70f6eb9853632fc7fc6755fa5349de0a&language=en-US&region=US&page=1')
+  .then(response => {
+    console.log(response)
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('error');
+});
+
+
