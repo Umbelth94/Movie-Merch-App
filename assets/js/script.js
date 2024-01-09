1bab8d800b72c06a8397aca4e2b7a3c8fee46a6c
Travis Umbel <Umbelth94@gmail.com>
2024-01-08 20:59:33 -0600

    added pseudo notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,4 @@
 
 //Displays locally saved movies
 
-  
-fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=70f6eb9853632fc7fc6755fa5349de0a&language=en-US&region=US&page=1')
-  .then(response => {
-    console.log(response)
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log('error');
-});
-
 
