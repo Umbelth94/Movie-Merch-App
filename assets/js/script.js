835c3dcf349edc83b9e54bffa9aa1da18fc21dc3
Travis Umbel <Umbelth94@gmail.com>
2024-01-08 21:01:34 -0600

    added pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,18 @@
 
 //Displays locally saved movies
 
+const API_KEY = "70f6eb9853632fc7fc6755fa5349de0a";
 
+fetch(
+    "https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY
+)
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log("error");
+    });
